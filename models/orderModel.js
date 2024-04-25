451f7192e2c4c7e898b196caaeaed1a982681bdf
Derjai <ojaider@uninorte.edu.co>
2024-04-25 10:12:27 -0500

    Added models and db connection - Created models for users, books and orders - Configured connection to MongoDB database - Added .gitignore file

---
+++ models/orderModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    sender: { type: Number, required: true},
+    date: { type: Date, required: true },
+    status: { type: String, enum: ['On Progress', 'Canceled', 'Completed'],default: 'On Progress', required: true },
+    details: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
+    total: { type: Number, required: true },
+    addressee: { type: Number, required: true },
+    deletedOn: {type: Date, required: false}
+}, { timestamps: true });
+
+const Order = mongoose.model('Orders', orderSchema);
+
+module.exports = Order;
